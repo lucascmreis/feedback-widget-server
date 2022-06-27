058f7bc0b6911f301a78b8e2a8a566bb9ca441a1
lucas.creis <lucas.creis@b2wdigital.com>
2022-06-27 23:14:16 +0200

    feat: create feedbacks route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import { prisma } from './prisma'
 
 const app = express()
+app.use(express.json())
 
-app.get('/api', (req, res) =>{
-  return res.send('hello')
+app.post('/feedbacks', async (req, res) =>{
+  const {type, comment, screenshot} = req.body
+
+ const feedback = await prisma.feedback.create({
+    data:{
+      type,
+      comment,
+      screenshot
+  }})
+  return res.status(201).json({data: feedback})
 })
 
 app.listen(3333, ()=>{
